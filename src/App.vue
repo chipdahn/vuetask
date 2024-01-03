4d1fb22e908204c0bb4286db5da91ac1442da28d
chipdahn <ibidapooluwapelumi@gmail.com>
2024-01-03 01:53:18 +0100

    deleted the default files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import Header from './components/Header.vue';
 import Sidebar from './components/Sidebar.vue'
 </script>
@@ -12,17 +10,7 @@ import Sidebar from './components/Sidebar.vue'
 
   </main>
   
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main> -->
+  
 </template>
 
 <style scoped>
